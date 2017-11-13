1f73c73e15ae5c4364b37cd5ccc371e4d7a5273b
Christian McCormack <christianjmccormack@gmail.com>
2017-11-13 20:26:44 +0000

    Added the test cout.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+
 	// introduce the game
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
@@ -18,6 +19,7 @@ int main()
 
 	//repeat the guess back to them
 	cout << "Your guess was: " << Guess << "." << endl;
+	cout << "This is a test";
 
 	//get a guess from the player
 	cout << "Please make a guess: ";
@@ -25,6 +27,8 @@ int main()
 
 	//repeat the guess back to them
 	cout << "Your guess was: " << Guess << "." << endl;
+
+	//This is a comment to prove Github is working
 
 	return 0;
 
