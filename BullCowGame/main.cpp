acd55de36be3e94462c96bb6a093826dfac3cb8a
Christian Mccormack <christianjmccormack@gmail.com>
2018-01-17 08:21:38 +0000

    Added the loop to iterate guesses.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -12,9 +12,14 @@ string GetGuessAndPrintBack();
 int main()
 {
 	PrintIntro();
-	
-	GetGuessAndPrintBack();
-	GetGuessAndPrintBack();
+
+	// Loop for the number of turns, asking for guesses.
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		GetGuessAndPrintBack();
+		cout << endl;
+	}
 	
 	return 0;
 }
@@ -22,21 +27,21 @@ int main()
 // introduce the game
 void PrintIntro()
 {
-
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 	return;
 }
 
-// get a guess from the player and repeat the guess back to them
+
 string GetGuessAndPrintBack()
 {
-
+	// get a guess from the player
 	string Guess = "";
 	cout << "Please make a guess: ";
 	getline(cin, Guess);
 
+	// Repeat the guess back to them
 	cout << "Your guess was: " << Guess << "." << endl;
 
 	return Guess;
