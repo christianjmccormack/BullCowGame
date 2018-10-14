935c3e2ecae5185a99b208cd06881db7a2ae4c50
Christian McCormack <christianjmccormack@gmail.com>
2018-10-14 17:52:25 +0100

    Organising with functions.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,34 +3,37 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// Entry point for our application.
 int main()
 {
+	PrintIntro();
+	GetGuessAndPrintBack();	
+	GetGuessAndPrintBack();
+
+	return 0;
+}
+
+// get a guess from the player
+string GetGuessAndPrintBack() {
+	// get a guess from the player
+	string Guess = "";
+	cout << "Please enter your guess: ";
+	getline(cin, Guess);
+	
+	// print the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
+}
+
+void PrintIntro() {
 	// introduce the game
 	constexpr int WORD_LENGTH = 6;
 	cout << "Welcome to Bulls and Cows, a fun word game" << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
-
-
-	// get a guess from the player
-	string Guess = "";
-	cout << "Please enter your guess: ";
-	cin >> Guess;
-
-
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-	   
-	// get a guess from the player
-	cout << "Please enter your guess: ";
-	cin >> Guess;
-
-
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-
-
-	return 0;
+	return;
 }
-
