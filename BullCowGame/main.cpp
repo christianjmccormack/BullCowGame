d91f297f1ae65a74332fcb30e9922f9f22a48c59
Christian McCormack <christianjmccormack@gmail.com>
2018-10-14 07:37:14 +0100

    Game input and Output

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-	std::cout << "This sucks" << std::endl;
+	// introduce the game
+	constexpr int WORD_LENGTH = 6;
+	cout << "Welcome to Bulls and Cows, a fun word game" << endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+
+	// get a guess from the player
+	string Guess = "";
+	cout << "Please enter your guess: ";
+	cin >> Guess;
+
+
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+	   
+	// get a guess from the player
+	cout << "Please enter your guess: ";
+	cin >> Guess;
+
+
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+
 	return 0;
 }
 
